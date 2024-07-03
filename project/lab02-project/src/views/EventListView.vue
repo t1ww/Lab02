dd392f0e70c19077ca0570318866c05ada97d1af
t1ww <95153968+t1ww@users.noreply.github.com>
2024-07-03 21:59:01 +0700

    commit 15.3

--- project/lab02-project/src/views/EventListView.vue
+++ project/lab02-project/src/views/EventListView.vue
@@ -1,43 +1,22 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
+import EventInfo from '@/components/Organize.vue'
 import Event from '@/types/Event'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
 
-const events = ref<Event[]>([
-  {
-    id: 5928101,
-    category: 'animal welfare',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend ay this event.',
-    location: 'Meow Town',
-    date: 'January 28, 2022',
-    time: '12.00',
-    petsAllowed: true,
-    organizer: 'Kay Laydee'
-  },
-  {
-    id: 4582797,
-    category: 'food',
-    title: 'Community Gardening',
-    description: 'Join us as we tend to the community edible plants.',
-    location: 'Flora City',
-    date: 'March 14, 2022',
-    time: '10.00',
-    petsAllowed: true,
-    organizer: 'Fern Pollin'
-  },
-  {
-    id: 8419988,
-    category: 'sustainability',
-    title: 'Beach Cleanup',
-    description: 'Help pick up trash along the shore.',
-    location: 'Playa Del carmen',
-    date: 'July 22, 2022',
-    time: '11.00',
-    petsAllowed: true,
-    organizer: 'Carey Wales'
-  }
-])
+const events = ref<Event[]>(null)
+
+onMounted(() => {
+  axios
+    .get('https://my-json-server.typicode.com/mzknkung53/652115008/events')
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
 </script>
 
 <template>
