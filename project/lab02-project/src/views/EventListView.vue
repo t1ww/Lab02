0a3f0d503aa362402abec4358595940e50a80f73
t1ww <nongtews@gmail.com>
2024-07-18 14:41:49 +0700

    2.7 and 2.8

--- project/lab02-project/src/views/EventListView.vue
+++ project/lab02-project/src/views/EventListView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
 import type { Event } from '@/type'
-import { ref, onMounted, watchEffect } from 'vue'
+import { ref, onMounted, watchEffect, computed } from 'vue'
 import EventService from '@/services/EventService'
 import type { AxiosResponse } from 'axios'
 
 const events = ref<Event[]>([])
+const totalEvent = ref<number>(0)
 const props = defineProps({
   page: {
     type: Number,
@@ -17,11 +18,17 @@ onMounted(() => {
     EventService.getEvents(2, props.page)
       .then((response: AxiosResponse<Event[]>) => {
         events.value = response.data
+        totalEvent.value = response.headers['x-total-count']
       })
       .catch((error) => {
         console.error('There was an error!', error)
       })
   })
+})
+const hasNextPage = computed(() => {
+  // calculate total page
+  const totalPages = Math.ceil(totalEvent.value / 2)
+  return props.page.valueOf() < totalPages
 })
 </script>
 
@@ -31,20 +38,24 @@ onMounted(() => {
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
     <EventInfo v-for="event in events" :key="event.id" :event="event"></EventInfo>
-    <RouterLink
-      :to="{ name: 'event-list-view', query: { page: page - 1 } }"
-      rel="prev"
-      v-if="page != 1"
-    >
-      Prev Page</RouterLink
-    >
-    <RouterLink
-      :to="{ name: 'event-list-view', query: { page: page + 1 } }"
-      rel="next"
-      v-if="page < 5"
-    >
-      Next Page</RouterLink
-    >
+    <div class="pagination">
+      <RouterLink
+        :to="{ name: 'event-list-view', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        id="page-prev"
+      >
+        Prev Page</RouterLink
+      >
+      <RouterLink
+        :to="{ name: 'event-list-view', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        id="page-next"
+      >
+        Next Page</RouterLink
+      >
+    </div>
   </div>
 </template>
 
@@ -53,5 +64,20 @@ onMounted(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+#page-prev {
+  text-align: left;
+}
+#page-next {
+  text-align: right;
 }
 </style>
