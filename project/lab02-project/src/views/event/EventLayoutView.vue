1e0b4dda254f65a22af0552700113e72e45db80e
t1ww <95153968+t1ww@users.noreply.github.com>
2024-08-07 21:58:04 +0700

    Update EventLayoutView.vue
    Fix

--- project/lab02-project/src/views/event/EventLayoutView.vue
+++ project/lab02-project/src/views/event/EventLayoutView.vue
@@ -5,13 +5,18 @@ import { storeToRefs } from 'pinia'
 const store = useEventStore()
 const { event } = storeToRefs(store)
 </script>
+
 <template>
   <div v-if="event">
     <h1>{{ event.title }}</h1>
     <div id="nav">
-      <RouterLink :to="{ name: 'event-detail-view', params: { id } }">Details</RouterLink> |
-      <RouterLink :to="{ name: 'event-register-view', params: { id } }">Register</RouterLink> |
-      <RouterLink :to="{ name: 'event-edit-view', params: { id } }">Edit</RouterLink>
+      <RouterLink :to="{ name: 'event-detail-view', params: { id: event.id } }">Details</RouterLink>
+      |
+      <RouterLink :to="{ name: 'event-register-view', params: { id: event.id } }"
+        >Register</RouterLink
+      >
+      |
+      <RouterLink :to="{ name: 'event-edit-view', params: { id: event.id } }">Edit</RouterLink>
     </div>
 
     <RouterView :event="event"></RouterView>
