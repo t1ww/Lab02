f15dad565e75c92c00932a447e809e3a3aa98109
t1ww <95153968+t1ww@users.noreply.github.com>
2024-08-29 20:55:43 +0700

    Lab07

--- project/lab02-project/src/services/EventService.ts
+++ project/lab02-project/src/services/EventService.ts
@@ -2,7 +2,7 @@ import axios, { type AxiosResponse } from 'axios'
 import type { Event } from '@/type'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/t1ww/lab02-mock-server/',
+  baseURL: 'http://localhost:8080/',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
