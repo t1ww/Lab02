680e567b4d54e60e8ffe078013ac33ff176e0b74
t1ww <95153968+t1ww@users.noreply.github.com>
2024-09-15 23:08:38 +0700

    7.5

--- project/lab02-project/src/router/index.ts
+++ project/lab02-project/src/router/index.ts
@@ -1,41 +1,28 @@
-// router/index.ts
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
 import AboutView from '@/views/AboutView.vue'
-import StudentListView from '@/views/StudentListView.vue'
-import EventDetailView from '@/views/event/EventDetailView.vue'
-import EventEditView from '@/views/event/EventEditView.vue'
-import EventRegisterView from '@/views/event/EventRegisterView.vue'
-import EventLayoutView from '@/views/event/EventLayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
-import NetworkErrorView from '@/views/NetworkErrorView.vue'
-import nProgress from 'nprogress'
-import EventService from '@/services/EventService'
-import { useEventStore } from '@/stores/event'
 
-export function createAppRouter(pageLimit: (number | null)[]) {
-  const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-      {
-        path: '/',
-        name: 'event-list-view',
-        component: EventListView,
-        props: (route) => ({
-          page: parseInt((route.query?.page as string) || '1'),
-          pageLimit
-        })
-      },
-      {
-        path: '/about',
-        name: 'about',
-        component: AboutView
-      },
-      {
-        path: '/student',
-        name: 'student',
-        component: StudentListView
-      },
+import EventDetailView from '@/views/event/EventDetailView.vue'
+import EventRegisterView from '@/views/event/EventRegisterView.vue'
+import EventEditView from '@/views/event/EventEditView.vue'
+import EventLayoutView from '@/views/event/EventLayoutView.vue'
+import AddEventView from '@/views/event/EventFormView.vue'
+
+import StudentListView from '@/views/StudentListView.vue'
+import nProgress from 'nprogress'
+import { useEventStore } from '@/stores/event'
+import EventService from '@/services/EventService'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'event-list-view',
+      component: EventListView,
+      props: (route) => ({ page: parseInt(route.query.page?.toString() || '1') })
+    },
       {
         path: '/event/:id',
         name: 'event-layout-view',
@@ -44,80 +31,84 @@ export function createAppRouter(pageLimit: (number | null)[]) {
         beforeEnter: (to) => {
           const id = parseInt(to.params.id as string)
           const eventStore = useEventStore()
-          return EventService.getEventById(id)
-            .then((response) => {
-              eventStore.setEvent(response.data)
-            })
-            .catch((error) => {
-              if (error.response && error.response.status === 404) {
-                return {
-                  name: '404-resource-view',
-                  params: { resourse: 'event' }
-                }
+          return EventService.getEvent(id)
+          .then((Response) => {
+            eventStore.setEvent(Response.data)
+          })
+          .catch((error) => {
+            if (error.Response && error.response.status === 404) {
+              return {
+                name: '404-resource-view',
+                params: { resource: 'event' }
               }
-            })
+            }else {
+              return { name: 'network-error-view'}
+            }
+          })
+      },
+      children: [
+        {
+          path: '',
+          name: 'event-detail-view',
+          component: EventDetailView,
+          props: true
         },
-        children: [
-          {
-            path: '',
-            name: 'event-detail-view',
-            component: EventDetailView,
-            props: true
-          },
-          {
-            path: 'edit',
-            name: 'event-edit-view',
-            props: true,
-            component: EventEditView
-          },
-          {
-            path: 'register',
-            name: 'event-register-view',
-            props: true,
-            component: EventRegisterView
-          }
-        ]
-      },
-      {
-        path: '/event/:id/edit',
-        name: 'event-edit',
-        props: true,
-        component: EventEditView
-      },
-      {
-        path: '/event/:id/register',
-        name: 'event-register',
-        props: true,
-        component: EventRegisterView
-      },
-      {
-        path: '/404/:resource',
-        name: '404-resource',
-        component: NotFoundView,
-        props: true
-      },
-      {
-        path: '/:catchAll(.*)',
-        name: 'not-found',
-        component: NotFoundView
-      },
-      {
-        path: '/network-error',
-        name: 'network-error',
-        component: NetworkErrorView
-      }
-    ],
-    scrollBehavior(to, from, savedPosition) {
-      if (savedPosition) {
-        return savedPosition
-      } else return { top: 0 }
+        {
+          path: 'register',
+          name: 'event-register-view',
+          component: EventRegisterView,
+          props: true
+        },
+        {
+          path: 'edit',
+          name: 'event-edit-view',
+          component: EventEditView,
+          props: true
+        }
+      ]
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/add-event',
+      name: 'add-event',
+      component: AddEventView
+    },
+    {
+      path: '/404/:resource',
+      name: '404-resource-view',
+      component: NotFoundView,
+      props: true
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
+    },
+    {
+      path: '/Student',
+      name: 'student',
+      component: StudentListView
     }
-  })
-  router.beforeEach(() => {
-    nProgress.start()
-  })
-  router.afterEach(() => {
-    nProgress.done()
-  })
-  return router
-}
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }else {
+    return { top: 0 }
+    }
+  }
+})
+
+router.beforeEach(() => {
+  nProgress.start()
+})
+
+router.afterEach(() => {
+  nProgress.done
+})
+
+export default router
