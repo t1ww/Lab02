e68e48e033b39058c70727740b38b385d66ee584
t1ww <95153968+t1ww@users.noreply.github.com>
2024-10-08 16:11:43 +0700

    q2 finish

--- project/lab02-project/src/views/event/DetailView.vue
+++ project/lab02-project/src/views/event/DetailView.vue
@@ -12,5 +12,8 @@ const { event } = toRefs(props)
     
         <p>{{ event.title }} @ {{ event.location }}</p>
         <p>{{ event.description }}</p>
-    
+    <div class="flex flex-row flex-warp justify-center">
+        <img v-for="image in event.images" :key="image" :src="image" alt="events image" 
+        class="border-solid border-gray-200 border-2 rounded p-1 m-1 w-40 hover:shadow-lg" />
+    </div>
 </template>
