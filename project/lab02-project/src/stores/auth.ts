b3f6e62158f2bf1c17774426bc39ee609000f633
t1ww <95153968+t1ww@users.noreply.github.com>
2024-10-10 16:33:57 +0700

    auth fix

--- project/lab02-project/src/stores/auth.ts
+++ project/lab02-project/src/stores/auth.ts
@@ -1,6 +1,8 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
 import type { AxiosInstance } from 'axios';
+import { Organizer } from '@/types';
+
 const apiClient: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: false,
@@ -12,7 +14,13 @@ const apiClient: AxiosInstance = axios.create({
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: null as string | null,
+    user: null as Organizer | null
   }),
+  getters: {
+    currentUserName(): string {
+      return this.user?.name || ''
+    }
+  },
   actions: {
     login(email: string, password: string) {
       return apiClient
@@ -22,7 +30,9 @@ export const useAuthStore = defineStore('auth', {
         })
         .then((response) => {
           this.token = response.data.access_token;
+          this.user = response.data.user;
           localStorage.setItem('access_token', this.token as string)
+          localStorage.setItem('user', JSON.stringify(this.user))
           return response;
         })
     }
